9d031183cf46ac3d5b0d42569905dda3c0c71fd4
Joa <ja.cutillas@iesdoctorbalmis.com>
2022-02-01 16:56:30 +0100

    BDServicio commit intermedio

---
+++ Aparcamiento Inteligente 2/servicios/DBServicio.cs
@@ -0,0 +1,30 @@
+﻿using Aparcamiento_Inteligente_2.modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aparcamiento_Inteligente_2.servicios
+{
+    class DBServicio
+    {
+        public string path { get; private set; }
+
+        public DBServicio(string path)
+        {
+            this.path = path;
+        }
+
+        public ObservableCollection<Cliente> ClientesGetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Vehiculo> VehiculosGetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
