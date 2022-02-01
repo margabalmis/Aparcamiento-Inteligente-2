6aff6502da85ed22e28ba615721b3079cb17b37a
Marga <71320648+margabalmis@users.noreply.github.com>
2022-02-01 08:36:14 +0100

    vehiculosBD

--- Aparcamiento Inteligente 2/servicios/VehiculosBD.cs
+++ Aparcamiento Inteligente 2/servicios/VehiculosBD.cs
@@ -18,12 +18,12 @@ namespace Aparcamiento_Inteligente_2.servicios
 
             ObservableCollection<Vehiculo> veiculosDB = new ObservableCollection<Vehiculo>();
 
-            veiculosDB.Add(new Vehiculo(1, 6 ,"María", "58698771V", "foto", 33, "x", "654985522"));
-            veiculosDB.Add(new Vehiculo(2, 2 ,"Julia", "11111171V", "fotos", 34, "x", "64524522"));
-            veiculosDB.Add(new Vehiculo(3, 1,"Ana", "58622222V", "foto", 35, "x", "651454522"));
-            veiculosDB.Add(new Vehiculo(4, 3,"Carmen", "58684671V", "foto", 31, "x", "6549254522"));
-            veiculosDB.Add(new Vehiculo(5, 4,"Esperanza", "888898771V", "foto", 53, "x", "654985555"));
-            veiculosDB.Add(new Vehiculo(6, 5,"Lucía", "58999991V", "foto", 43, "x", "6549835454"));
+            veiculosDB.Add(new Vehiculo(1, 6 , "58698771V", 33, "x", "coche"));
+            veiculosDB.Add(new Vehiculo(2, 2 , "11111171V",  34, "x", "moto"));
+            veiculosDB.Add(new Vehiculo(3, 1, "58622222V",  35, "y", "coche"));
+            veiculosDB.Add(new Vehiculo(4, 3, "58684671V",  31, "y", "coche"));
+            veiculosDB.Add(new Vehiculo(5, 4, "888898771V", 53, "x", "moto"));
+            veiculosDB.Add(new Vehiculo(6, 5, "58999991V", 43, "x", "moto"));
 
             return veiculosDB;
 
