021f679fd406a06c7c9c794c62a4fb14e7ff4de5
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2022-02-03 12:22:47 +0100

    Vistas modelo en proceso

--- Aparcamiento Inteligente 2/vistas/EditarVehiculoWindow.xaml.cs
+++ Aparcamiento Inteligente 2/vistas/EditarVehiculoWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aparcamiento_Inteligente_2.vistas_modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,12 @@ namespace Aparcamiento_Inteligente_2.vistas
     /// </summary>
     public partial class EditarVehiculoWindow : Window
     {
+        EditarVehiculoMV vm;
         public EditarVehiculoWindow()
         {
             InitializeComponent();
+            vm = new EditarVehiculoMV();
+            this.DataContext = vm;
         }
     }
 }
--- Aparcamiento Inteligente 2/vistas_modelos/EditarVehiculoMV.cs
+++ Aparcamiento Inteligente 2/vistas_modelos/EditarVehiculoMV.cs
@@ -4,6 +4,7 @@ using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,15 @@ namespace Aparcamiento_Inteligente_2.vistas_modelos
 {
     class EditarVehiculoMV : ObservableRecipient
     {
+        DBServicio baseDatos;
         public EditarVehiculoMV() 
         {
             VehiculoSeleccionado = WeakReferenceMessenger.Default.Send<VehiculoSeleccionadoMessage>();
+
+            // Cargar datos Marcas
+            baseDatos = new DBServicio();
+            Marcas = new ObservableCollection<Marcas>();
+            Marcas = baseDatos.MarcasGetAll();
         }
 
         private Vehiculo vehiculoSeleccionado;
@@ -24,6 +31,23 @@ namespace Aparcamiento_Inteligente_2.vistas_modelos
             get { return vehiculoSeleccionado; }
             set { SetProperty(ref vehiculoSeleccionado, value); }
         }
+
+        private Marcas marcaSeleccionado;
+
+        public Marcas MarcaSeleccionado
+        {
+            get { return marcaSeleccionado; }
+            set { SetProperty(ref marcaSeleccionado, value); }
+        }
+
+        private ObservableCollection<Marcas> marcas;
+
+        public ObservableCollection<Marcas> Marcas
+        {
+            get { return marcas; }
+            set { SetProperty(ref marcas, value); }
+        }
+
 
 
     }
