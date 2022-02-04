26d58c4e905984e6dfda391027840093e39ad54d
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2022-02-04 11:58:50 +0100

    Corregido error de binding

--- Aparcamiento Inteligente 2/vistas_modelos/EditarClienteWindowMV.cs
+++ Aparcamiento Inteligente 2/vistas_modelos/EditarClienteWindowMV.cs
@@ -1,6 +1,7 @@
 ﻿using Aparcamiento_Inteligente_2.modelo;
 using Aparcamiento_Inteligente_2.servicios;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using System;
 using System.Collections.Generic;
@@ -13,13 +14,32 @@ namespace Aparcamiento_Inteligente_2.vistas_modelos
     class EditarClienteWindowMV : ObservableRecipient
     {
         //Servicios
+        private readonly DialogosNavegacion servicioDialogos;
         readonly DBServicio baseDatos;
         public EditarClienteWindowMV()
         {
+            //Instancia Servicios
             baseDatos = new DBServicio();
+            servicioDialogos = new DialogosNavegacion();
             //Comunicación
             ClienteSeleccionado = WeakReferenceMessenger.Default.Send<ClienteSeleccionadoMessage>();
-          
+
+            ImagenCommand = new RelayCommand(AbrirImagen);
+
+        }
+        public RelayCommand ImagenCommand { get; }
+
+        private void AbrirImagen()
+        {
+            Foto = servicioDialogos.DialogoAbrirImagen();
+
+        }
+        private string foto;
+
+        public string Foto
+        {
+            get { return foto; }
+            set { SetProperty(ref foto, value); }
         }
 
         internal void EditarCliente()
