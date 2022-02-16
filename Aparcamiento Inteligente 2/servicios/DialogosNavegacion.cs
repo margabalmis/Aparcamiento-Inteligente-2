5a504839bdcf3279a019f01f3cfb05c05b4013bd
JAcutillas <ja.cutillas@iesdoctorbalmis.com>
2022-02-16 11:10:25 +0100

    Documentación servicio navegación

--- Aparcamiento Inteligente 2/servicios/DialogosNavegacion.cs
+++ Aparcamiento Inteligente 2/servicios/DialogosNavegacion.cs
@@ -9,38 +9,68 @@ using System.Windows;
 
 namespace Aparcamiento_Inteligente_2.servicios
 {
+    /// <summary>
+    /// Servicio de navegación
+    /// </summary>
     class DialogosNavegacion
     {
+        /// <summary>
+        /// Instancia y lanza el dialogo para añadir cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoAñadirCliente()
         {
             AñadirClienteWindow nuevaVentana = new AñadirClienteWindow();
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+        /// <summary>
+        /// Instancia y lanza el dialogo para editar un cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoEditarCliente()
         {
             EditarClienteWindow nuevaVentana = new EditarClienteWindow();
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+        /// <summary>
+        /// Instancia y lanza el dialogo para eliminar un  cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoEliminarCliente()
         {
             EliminarClienteWindow nuevaVentana = new EliminarClienteWindow();
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para añadir un nuevo Cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoAñadirVehiculo()
         {
             AñadirVehiculoWindow nuevaVentana = new AñadirVehiculoWindow();
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para añadir una nueva Marca
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoAñadirMarca()
         {
             AñadirMarcaW nuevaVentana = new AñadirMarcaW();
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para editar un vehículo desde la pestaña de clientes
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool)</returns>
         internal bool? DialogoEditarVehiculoDesdeCliente()
         {
             string origen = "cliente";
@@ -48,6 +78,11 @@ namespace Aparcamiento_Inteligente_2.servicios
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para editar un vehículo desde la pestaña de vehículo
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool?)</returns>
         internal bool? DialogoEditarVehiculoDesdeVehiculo()
         {
             string origen = "vehiculo";
@@ -55,6 +90,11 @@ namespace Aparcamiento_Inteligente_2.servicios
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para eliminar un vehiculo desde la pestaña de cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool?)</returns>
         internal bool? DialogoEliminarVehiculoDesdeCliente()
         {
             string origen = "cliente";
@@ -62,6 +102,11 @@ namespace Aparcamiento_Inteligente_2.servicios
             nuevaVentana.ShowDialog();
             return nuevaVentana.DialogResult;
         }
+
+        /// <summary>
+        /// Instancia y lanza el dialogo para  eliminar un vehiculo desde la pestaña de cliente
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool?)</returns>
         internal bool? DialogoEliminarVehiculoDesdeVehiculo()
         {
             string origen = "vehiculo";
@@ -70,11 +115,19 @@ namespace Aparcamiento_Inteligente_2.servicios
             return nuevaVentana.DialogResult;
         }
 
+        /// <summary>
+        /// Instancia y lanza el dialogo para finalizar un estacionamiento
+        /// </summary>
+        /// <returns>Devuelve el resultado del dialogo (bool?)</returns>
         internal bool? MessageBoxFinalizarEstacionamiento()
         {
             return MessageBoxResult.Yes == MessageBox.Show("¿Quieres finalizar el estacionamiento?", 
                 "Finalizar estacionamiento", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
         }
+        /// <summary>
+        /// Instancia y lanza el dialogo para abrir una imagen
+        /// </summary>
+        /// <returns>Devuelve la ruta de la imagen en string4</returns>
         internal string DialogoAbrirImagen()
         {
             string foto = null;
